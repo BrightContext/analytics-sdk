f5470e44eeb276df5bdabdc01a52e17029940638
sfusco <sfusco@0e9b7cb4-b789-4f24-a7da-c35d214e520d>
2014-03-25 15:14:49 +0000

    grunt compile
    git-svn-id: svn://svn.bcclabs.com/productrepo/sdk/trunk/analytics@26082 0e9b7cb4-b789-4f24-a7da-c35d214e520d

--- build/bcc-analytics.js
+++ build/bcc-analytics.js
@@ -9,11 +9,11 @@
         b && b.length > 1 ? (c = b[1], c && (c = c.replace(/^\s+|\s+$/gm, "")), "" === c ? null : c) : null;
     }
     function c() {
-        var c = b(y);
+        var c = b(I);
         return c || (c = a(), d(c)), c;
     }
     function d(a) {
-        document.cookie = y + "=" + a;
+        document.cookie = I + "=" + a;
     }
     function e(a) {
         if (!a) return {};
@@ -30,7 +30,7 @@
         return a ? void 0 !== a.pageY ? a.pageY : void 0 !== a.clientY && document.documentElement ? a.clientY + document.documentElement.scrollTop : 0 : 0;
     }
     function h() {
-        return v.performance && window.performance ? window.performance : {};
+        return F.performance && window.performance ? window.performance : {};
     }
     function i(a, b, c) {
         a.addEventListener ? a.addEventListener(b, c, !1) : a.attachEvent && a.attachEvent("on" + b, c);
@@ -40,128 +40,197 @@
         for (d in f) d && (e = f[d], i(e, b, c));
     }
     function k(a, b) {
+        try {
+            var c = new XMLHttpRequest();
+            return "withCredentials" in c ? c.open(a, b, !0) : "undefined" != typeof XDomainRequest ? (c = new XDomainRequest(), 
+            c.open(a, b)) : c = null, c && c.setRequestHeader("Content-Type", "application/json"), 
+            c;
+        } catch (d) {
+            return console.log(d), null;
+        }
+    }
+    function l(a, b) {
         if (a) try {
-            var c, d = window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
-            c = JSON.stringify(b), d.open("POST", a, !0), d.setRequestHeader("Content-Type", "application/json"), 
-            d.send(c);
+            var c = JSON.stringify(b), d = k("POST", a);
+            d ? (console.log(c), d.send(c)) : console.error("cors not available");
         } catch (e) {}
     }
-    function l() {
+    function m() {
         var a = document.querySelectorAll('link[rel="canonical"]');
         return a && a.length && "href" in a[0] && "" !== a[0].href ? a[0].href : window.location.href;
     }
-    function m() {
-        var a = 0;
-        i(window, "scroll", function() {
-            a += 1;
-        }), setInterval(function() {
-            z.scroll = a;
-            var b = 0 !== z.scroll || 0 === z.scroll && 0 !== z.prevscroll;
-            b && k(w, {
-                type: "scroll",
+    function n() {
+        return document.referrer || "";
+    }
+    function o() {
+        var a = document.querySelectorAll("title");
+        return a && a.length ? a[0].innerHTML : "";
+    }
+    function p(a, b) {
+        var c = document.querySelectorAll(a);
+        return c && c.length && b in c[0] ? c[0][b] : 0;
+    }
+    function q() {
+        return p("body", "scrollHeight");
+    }
+    function r() {
+        return p("body", "scrollWidth");
+    }
+    function s() {
+        return p("body", "scrollTop");
+    }
+    function t() {
+        return p("body", "scrollTop") + window.innerHeight;
+    }
+    function u() {
+        return p("body", "scrollLeft");
+    }
+    function v() {
+        return p("body", "scrollLeft") + window.innerWidth;
+    }
+    function w() {
+        var a = {
+            pageheight: 0,
+            pagewidth: 0,
+            viewabletop: 0,
+            viewablebottom: 0,
+            viewableleft: 0,
+            viewableright: 0
+        };
+        setInterval(function() {
+            var b = {
+                pageheight: q(),
+                pagewidth: r(),
+                viewabletop: s(),
+                viewablebottom: t(),
+                viewableleft: u(),
+                viewableright: v()
+            }, c = b.pageheight !== a.pageheight || b.pagewidth !== a.pagewidth || b.viewabletop !== a.viewabletop || b.viewablebottom !== a.viewablebottom || b.viewableleft !== a.viewableleft || b.viewableright !== a.viewableright;
+            c && l(G, {
+                type: "position",
                 milestonename: "",
                 milestoneobj: {},
-                baseurl: B.canonical,
+                baseurl: K.canonical,
                 fullurl: window.location.href,
-                pagename: B.name,
-                tags: B.tags,
-                fullref: document.referrer || "",
-                userid: A.id,
-                userobj: A.details,
+                pagename: K.name,
+                tags: K.tags,
+                fullref: n(),
+                userid: J.id,
+                userobj: J.details,
                 element: {},
                 mousex: 0,
                 mousey: 0,
-                scrollspeed: z.scroll,
+                pagetitle: o(),
+                pageheight: q(),
+                pagewidth: r(),
+                viewabletop: s(),
+                viewablebottom: t(),
+                viewableleft: u(),
+                viewableright: v(),
                 performobj: h()
-            }), z.prevscroll = a, a = 0;
+            }), a = b;
         }, 5e3);
     }
-    function n() {
+    function x() {
         j("body", "click", function(a) {
-            k(w, {
+            l(G, {
                 type: "click",
                 milestonename: "",
                 milestoneobj: {},
-                baseurl: B.canonical,
+                baseurl: K.canonical,
                 fullurl: window.location.href,
-                pagename: B.name,
-                tags: B.tags,
-                fullref: document.referrer || "",
-                userid: A.id,
-                userobj: A.details,
+                pagename: K.name,
+                tags: K.tags,
+                fullref: n(),
+                userid: J.id,
+                userobj: J.details,
                 element: e(a),
                 mousex: f(a),
                 mousey: g(a),
-                scrollspeed: 0,
+                pagetitle: o(),
+                pageheight: q(),
+                pagewidth: r(),
+                viewabletop: s(),
+                viewablebottom: t(),
+                viewableleft: u(),
+                viewableright: v(),
                 performobj: h()
             });
         });
     }
-    function o() {
+    function y() {
         var a, b;
         j("body", "click", function(c) {
             c.target && (a = c.target.getAttribute("data-bcc-milestone-name"), b = c.target.getAttribute("data-bcc-milestone-details") || {}, 
-            a && r(a, b, c));
+            a && B(a, b, c));
         });
     }
-    function p() {
+    function z() {
         return window.location.href.replace(/:\/\/.*/, "");
     }
-    function q(a, b) {
-        v = b, b.environment && (x = b.environment), w = p() + "://" + x + "/da/" + a, i(document, "DOMContentLoaded", function() {
-            B.canonical = l(), A.id = c(), n(), o(), b && (b.movement && m(), b.environment && (x = b.environment), 
-            t(b.page), u(b.tags), k(w, {
+    function A(a, b) {
+        F = b, b.environment && (H = b.environment), G = z() + "://" + H + "/da/" + a, i(document, "DOMContentLoaded", function() {
+            K.canonical = m(), J.id = c(), x(), y(), b && (b.movement && w(), b.environment && (H = b.environment), 
+            D(b.page), E(b.tags), l(G, {
                 type: "pageload",
                 milestonename: "",
                 milestoneobj: {},
-                baseurl: B.canonical,
+                baseurl: K.canonical,
                 fullurl: window.location.href,
-                pagename: B.name,
-                tags: B.tags,
-                fullref: document.referrer || "",
-                userid: A.id,
-                userobj: A.details,
+                pagename: K.name,
+                tags: K.tags,
+                fullref: n(),
+                userid: J.id,
+                userobj: J.details,
                 element: {},
                 mousex: 0,
                 mousey: 0,
-                scrollspeed: 0,
+                pagetitle: o(),
+                pageheight: q(),
+                pagewidth: r(),
+                viewabletop: s(),
+                viewablebottom: t(),
+                viewableleft: u(),
+                viewableright: v(),
                 performobj: h()
             }));
         });
     }
-    function r(a, b, c) {
-        k(w, {
+    function B(a, b, c) {
+        l(G, {
             type: "milestone",
             milestonename: a,
             milestoneobj: b || {},
-            baseurl: B.canonical,
+            baseurl: K.canonical,
             fullurl: window.location.href,
-            pagename: B.name,
-            tags: B.tags,
-            fullref: document.referrer || "",
-            userid: A.id,
-            userobj: A.details,
+            pagename: K.name,
+            tags: K.tags,
+            fullref: n(),
+            userid: J.id,
+            userobj: J.details,
             element: e(c),
             mousex: f(c),
             mousey: g(c),
-            scrollspeed: 0,
+            pagetitle: o(),
+            pageheight: q(),
+            pagewidth: r(),
+            viewabletop: s(),
+            viewablebottom: t(),
+            viewableleft: u(),
+            viewableright: v(),
             performobj: h()
         });
     }
-    function s(a) {
-        return void 0 !== a && (A.details = a), A.details;
+    function C(a) {
+        return void 0 !== a && (J.details = a), J.details;
     }
-    function t(a) {
-        return void 0 !== a && (B.name = a || ""), B.name;
+    function D(a) {
+        return void 0 !== a && (K.name = a || ""), K.name;
     }
-    function u(a) {
-        return void 0 !== a && (B.tags = a || []), B.tags;
+    function E(a) {
+        return void 0 !== a && (K.tags = a || []), K.tags;
     }
-    var v, w, x = "pub.brightcontext.com", y = "bcau", z = {
-        url: "",
-        prevscroll: 0,
-        scroll: 0
-    }, A = {}, B = {};
+    var F, G, H = "pub.brightcontext.com", I = "bcau", J = {}, K = {};
     document.querySelectorAll || (document.querySelectorAll = function(a) {
         var b, c = document.createElement("style"), d = [];
         for (document.documentElement.firstChild.appendChild(c), document._qsa = [], c.styleSheet.cssText = a + "{x-qsa:expression(document._qsa && document._qsa.push(this))}", 
@@ -176,6 +245,6 @@
         e.push((f ? "" : '"' + c + '":') + String(d)));
         return (f ? "[" : "{") + String(e) + (f ? "]" : "}");
     }), window.BCC || (window.BCC = {}), window.BCC.Analytics || (window.BCC.Analytics = {}), 
-    window.BCC.Analytics.init = q, window.BCC.Analytics.track = r, window.BCC.Analytics.identify = s, 
-    window.BCC.Analytics.page = t, window.BCC.Analytics.tags = u;
+    window.BCC.Analytics.init = A, window.BCC.Analytics.track = B, window.BCC.Analytics.identify = C, 
+    window.BCC.Analytics.page = D, window.BCC.Analytics.tags = E;
 }();
